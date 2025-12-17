d27f5b0296d8216ccb9791afafaf44a268e5d020
Rasha Atteya <61626646+rasha-atteya@users.noreply.github.com>
2025-12-17 11:41:04 +0200

    Replace train.csv with iris.csv for analysis
    Removed loading of 'train.csv' and added loading of 'iris.csv' for data visualization.

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ st.write("hello World")
 x=st.text_input("Favourite Movie?")
 st.write(f" Your favourite movie is {x}")
 import pandas as pd
-df=pd.read_csv('train.csv')
-st.write(df)
+
 df1=pd.read_csv('iris.csv')
 st.write(df1)
 st.bar_chart(df1)
